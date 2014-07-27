9589e03ce074d5d216483632694801c1e71b66c0
martin.schalk@googlemail.com <martin.schalk@googlemail.com@e600db9b-bde8-7d95-8fc8-824e3ca91893>
2014-07-27 19:00:59 +0000

    - prototype of bus implemented

---
+++ trunk/Servo/SW/ServoMotor/ServoMotor.cydsn/bus.h
@@ -0,0 +1,44 @@
+/* Copyright (C) <2014> <Martin Schalk>
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+
+#include <project.h>
+#include "global.h"
+
+
+#define BUS_MSG_BUFFER_SIZE	16
+#define BUS_LENGTH_ERROR	-1
+
+typedef struct _bus_t_obj
+{
+	uint8* 	tBuffer;
+	uint8	tBytes;
+	BOOL	tRequest;
+	BOOL	tInProgress;
+}T_OBJ;
+
+
+typedef struct _bus_r_obj
+{
+	uint8* 	rBuffer;
+	uint8	rBytes;
+	BOOL	rNewMsg;
+	BOOL	rInProgress;
+}R_OBJ;
+
+
+typedef struct _bus
+{
+	T_OBJ	transmit;
+	R_OBJ	receive;
+}BUS_OBJ;
+
+extern BUS_OBJ Bus;
+
+STATUS BUS_Init(void);
+STATUS BUS_Handler(void);
+
+/* [] END OF FILE */
--- trunk/Servo/SW/ServoMotor/ServoMotor.cydsn/main.c
+++ trunk/Servo/SW/ServoMotor/ServoMotor.cydsn/main.c
@@ -1,24 +1,29 @@
-/* ========================================
+/* Copyright (C) <2014> <Martin Schalk>
+ * All rights reserved.
  *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ */
+
 #include <project.h>
+#include "servo_cds5500.h"
+#include "bus.h"
+#include "global.h"
 
 int main()
 {
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+	STATUS status; 
+    /* Initialization */
+	BUS_Init();
+	UART_SERVO_Init();
+	UART_SERVO_EnableRxInt();
+	UART_SERVO_Start();
+	
     /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
     for(;;)
     {
-        /* Place your application code here. */
+        /* Bus handler */
+		status = BUS_Handler();
     }
 }
 
